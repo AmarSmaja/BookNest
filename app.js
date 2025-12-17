56a1e0c0ddbac9b6a3305042390c8f2ab3a97e90
Amar Smaic <amarsmaic@gmail.com>
2025-12-17 18:03:58 +0100

    connection to db

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+require("dotenv").config();
+
+const db = require("./models")
+
+db.sequelize.authenticate().then(() => console.log("Konektovano na bazu")).catch((err) => console.error("Error pri konekciji: ", err))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
---
+++ models/index.js
@@ -0,0 +1,7 @@
+const sequelize = require("../config/db");
+
+const db = {
+    sequelize, 
+};
+
+module.exports = db
